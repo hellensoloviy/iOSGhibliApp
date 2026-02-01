4ccf870a70f81ab2dc6bb87230e013a116db9bb9
Hellen S <hellensoloviy@gmail.com>
2026-02-01 11:29:40 +0200

    Initial Commit

---
+++ ChibliApp/ChibliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChibliAppApp.swift
+//  ChibliApp
+//
+//  Created by Olena Solovii AFT on 01.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChibliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChibliAppTests/ChibliAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChibliAppTests.swift
+//  ChibliAppTests
+//
+//  Created by Olena Solovii AFT on 01.02.2026.
+//
+
+import Testing
+@testable import ChibliApp
+
+struct ChibliAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
